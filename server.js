51d356a548b52ab441f603db9fae9ede506947dd
a-strange-octopus <39961132+a-strange-octopus@users.noreply.github.com>
2019-05-20 18:49:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', (req, res) => res.sendFile('/index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
