8030644e8bebf7d7fb8e964fbdf956b85f11bd32
a-strange-octopus <39961132+a-strange-octopus@users.noreply.github.com>
2019-05-20 18:25:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var path = require('path')
 const port = process.env.PORT
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
